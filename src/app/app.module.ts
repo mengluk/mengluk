f9c91e22de2472cf7ae9724b049191a930bc7469
mengluk <meng.luk@hotmail.com>
2021-04-04 15:02:41 +0700

    Enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//import FontAwsomeModule
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome'
+import { faUser, faLock, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook,faGoogle,faApple } from '@fortawesome/free-brands-svg-icons';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary) {
+ 
+    // Add an icon to the library for convenient access in other components
+    library.addIcons(faUser,faLock,faPhone,faFacebook,faGoogle,faApple);
+  }
+}
